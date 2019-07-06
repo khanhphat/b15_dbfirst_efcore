3dc3bbaa40b6155d6b170ecf2a8e794d6647bd37
khanhphat <nguyenhuukhanhphat@gmail.com>
2019-07-06 17:24:17 +0700

    - Thuc hanh LinQ - Thong ke - Phan Trang

---
+++ b15_dbfirst_efcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace b15_dbfirst_efcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
